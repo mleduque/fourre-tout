40bff73ba6648b895dea0183aab90739cc364603
Mickaël Leduque <mleduque@gmail.com>
2019-02-14 14:39:03 +0100

    create table: models

--- src/schema.rs
+++ src/schema.rs
@@ -9,3 +9,20 @@ table! {
         first_published -> Timestamptz,
     }
 }
+
+table! {
+    models (id) {
+        id -> Int8,
+        version -> Int8,
+        name -> Varchar,
+        old_version -> Nullable<Int8>,
+        body -> Jsonb,
+        published -> Timestamptz,
+        first_published -> Timestamptz,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    documents,
+    models,
+);
